bf42fc30980017934cafd16669bbc1bbfd7e1bb4
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-03-09 12:31:50 +0500

    feat: change vercel config

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const app = express();
 app.get('/', (_, res) => res.status(200).json({ message: 'Hello World' }));
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+module.exports = app;
