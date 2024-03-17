abef050ebff145fb7723312da2e3c408c2279884
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2024-03-17 14:54:36 +0500

    Add route to fetch group messages by groupId

--- routes/group.route.js
+++ routes/group.route.js
@@ -4,6 +4,18 @@ const router = express.Router()
 
 const db = require("../models")
 
+router.get(`/messages/:groupId`, async (req, res) => {
+  const { groupId } = req.params
+
+  if (!groupId) {
+    return res.status(400).json({ message: `groupId is required` })
+  }
+
+  const groupMessages = await db.groupMessage.find({ groupId })
+
+  res.status(200).json(groupMessages)
+})
+
 router.get("/:userId", async (req, res) => {
   const { userId } = req.params
 
